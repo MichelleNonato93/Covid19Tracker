9979e7d0cf25589218f93387947d1243122e12ee
MichelleNonato93 <michelle.nonato@gmail.com>
2022-03-24 11:33:09 +0800

    changes again

--- CVD19Tracker/WorldViewController.swift
+++ CVD19Tracker/WorldViewController.swift
@@ -1,53 +1,7 @@
-//  WorldViewController.swift
-//  CVD19Tracker
-//
-
 import UIKit
 
+var greeting = "Hello, playground"
 
-class WorldViewController: UIViewController {
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-        
-        //Note: Background Color, fonts and text colors for labels, buttons and background
-        view.backgroundColor = #colorLiteral(red: 0.3411764706, green: 0.2823529412, blue: 0.6196078431, alpha: 1)
-        
-        let url = "https://api.apify.com/v2/key-value-stores/tVaYRsPHLjNdNBu7S/records/LATEST?disableRedirect=true"
-        getCovidData(from: url)
-    }
-    
-    private func getCovidData(from url: String) {
-        let task = URLSession.shared.dataTask(with: URL(string: url)!, completionHandler: {data, response, error in
-
-           guard let error = error else {
-            print("ERROR: \(error)")
-            return
-           }
-           
-           guard let data = data else {
-             return
-           }
-           
-           print("RESPONSE: \(response) ")
-           
-           do {
-             let json = try JSONDecoder().decode(JsonAPI.self, from: data)
-               print(json.results)
-                    
-                    
-           } catch {
-                   print("Error \(error.localizedDescription)")
-               }
-               
-           })
-           
-           task.resume()
-           
-       }
-   }
 
 struct JsonAPI: Decodable {
     let results: [JsonResult]
@@ -55,7 +9,6 @@ struct JsonAPI: Decodable {
 
 struct JsonResult: Decodable {
     var infected: Int
-    var tested: Int?
     var recovered: Int?
     var deceased: Int
     var country: String
@@ -63,4 +16,31 @@ struct JsonResult: Decodable {
     var historyData: URL
     var sourceUrl: URL
     var lastUpdatedApify: Date
+}
+
+let url = "https://api.apify.com/v2/key-value-stores/tVaYRsPHLjNdNBu7S/records/LATEST?disableRedirect=true"
+getCovidData(from: url)
+
+private func getCovidData(from url: String) {
+    let task = URLSession.shared.dataTask(with: URL(string: url)!, completionHandler: {data, response, error in
+
+       guard let error = error else {
+        print("ERROR: \(error)")
+        return
+       }
+       
+       guard let data = data else {
+         return
+       }
+       
+       print("RESPONSE: \(response) ")
+       
+       do {
+         let json = try JSONDecoder().decode(JsonAPI.self, from: data)
+           print(json)
+       } catch {
+               print("Error \(error.localizedDescription)")
+           }
+           
+    }).resume()
 }
